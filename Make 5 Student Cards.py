27715a33bbfb9fd1956a5a625f524b6b0e4cc024
Mir Abdulrehman <49989324+MirAbdulRehman@users.noreply.github.com>
2019-10-28 19:47:09 +0500

    Making 5 Student Cards
    Using 5 pre-defined functions of Python I have created 5 Student Cards as an example.

--- Make 5 Student Cards.py
+++ Make 5 Student Cards.py
@@ -1,10 +1,14 @@
 name1 = "Mir AbdulRehman"
 name2 = "Ahmad"
 name3 = "Waqas"
+name4 = "Ali"
+name5 = "Qasim"
 
 father_name1 = "Sohail Mir"
 father_name2 = "Junaid"
 father_name3 = "Sharoz"
+father_name4 = "Kashif"
+father_name5 = "Hammad"
 
 university = "Bahria University"
 
@@ -26,6 +30,20 @@ Father Name: {}
 University: {}
 """.format(name3,father_name3,university)
 
+card4 = """
+Name: {}
+Father Name: {}
+University: {}
+""".format(name4,father_name4,university)
+
+card5 = """
+Name: {}
+Father Name: {}
+University: {}
+""".format(name5,father_name5,university)
+
 print(card1)
 print(card2)
 print(card3)
+print(card4)
+print(card5)
